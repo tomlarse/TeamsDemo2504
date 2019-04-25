89c75f2fcd9ac10fcfc0f7bdadbea6913457e283
Tom-Inge Larsen <tom-inge.larsen@rewired.no>
2019-04-25 15:04:23 +0200

    sd

--- src/CreateTeams.ps1
+++ src/CreateTeams.ps1
@@ -14,7 +14,7 @@ $SPList = "Nytt Team"
 $login = Get-AutomationPSCredential -Name 'AzureAdmin'
 
 Login-AzureRmAccount -Credential $login
-$tenantId = Get-AzureRmAutomationVariable Name 'TenantId'
+$tenantId = Get-AzureRmAutomationVariable Name 'TenantId' -AutomationAccountName 'TeamsDemo'
 
 Import-Module SharePointPnPPowerShellOnline
 
